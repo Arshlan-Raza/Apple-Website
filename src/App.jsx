e8802b00002a773574058ec4830fdb55c674b2e8
Arshlan Raza <98450417+Arshlan-Raza@users.noreply.github.com>
2025-02-24 22:56:19 +0530

    completed the iphone 15  website

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 
+import * as Sentry from '@sentry/react';
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
+
 const App = () => {
  
 
@@ -13,8 +18,11 @@ const App = () => {
     <Hero/>
     <Highlights/>
     <Model/>
+    <Features/>
+    <HowItWorks/>
+    <Footer/>
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App); 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://7669d88da8275f086cb22a394f3c38ef@o4508874979540992.ingest.us.sentry.io/4508874983211008",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+const container = document.getElementById("app");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
